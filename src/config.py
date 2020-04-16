5e98235833193c6b5cb66d65a7051091082152e4
sam246 <shiraz.amin@uky.edu>
2020-04-16 16:26:58 -0400

    initial commit

---
+++ src/config.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+DEBUG = os.getenv("ENVIRONEMENT") == "DEV"
+APPLICATION_ROOT = os.getenv("APPLICATION_APPLICATION_ROOT", "/application")
+HOST = os.getenv("APPLICATION_HOST")
+PORT = int(os.getenv("APPLICATION_PORT", "3000"))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DB_CONTAINER = os.getenv("APPLICATION_DB_CONTAINER", "db")
+POSTGRES = {
+    "user": os.getenv("MYSQL_USER", "root"),
+    "pw": os.getenv("MYSQL_PASSWORD", ""),
+    "host": DB_CONTAINER,
+    "port": os.getenv("APPLICATION_MYSQL_PORT", 3032),
+    "db": os.getenv("MYSQL_DATABASE", "mysql"),
+}
+
+DB_URI = "mysql+mysqldb://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s" % POSTGRES
+
+logging.basicConfig(
+    filename=os.getenv("SERVICE_LOG", "server.log"),
+    level=logging.DEBUG,
+    format="%(levelname)s: %(asctime)s \
+        pid:%(process)s module:%(module)s %(message)s",
+    datefmt="%d/%m/%y %H:%M:%S",
+)
