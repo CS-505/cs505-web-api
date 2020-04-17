f540a99f47a8755694d274edd7fd3e241b10e630
Shiraz Amin <shiraz@shirazamin.com>
2020-04-17 13:25:17 -0400

    added reset end point

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserResource
 from .management_team import TeamResource
+from .management_reset import ResetResource
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,2 +1,3 @@
 from .user import USER_BLUEPRINT
 from .management_team import TEAM_BLUEPRINT
+from .management_reset import RESET_BLUEPRINT
