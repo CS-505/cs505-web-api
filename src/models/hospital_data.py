486e4d58793fdbc3a5329496f278629fb0886812
ryanbowles <ryanbowles42@gmail.com>
2020-04-24 16:07:37 -0400

    added patient table, added trauma_level to hospital table

--- src/models/hospital_data.py
+++ src/models/hospital_data.py
@@ -15,14 +15,16 @@ class HospitalData(db.Model, BaseModel, metaclass=MetaBaseModel):
     beds_total = db.Column(db.Integer())
     beds_occupied = db.Column(db.Integer())
     beds_available = db.Column(db.Integer())
+    trauma_level = db.Column(db.Integer())
     
 
-    def __init__(self, hospital_id, hospital_zip, beds_total, beds_occupied, beds_available):
+    def __init__(self, hospital_id, hospital_zip, beds_total, beds_occupied, beds_available, trauma_level):
         """ Create a new record """
         self.hospital_id = hospital_id
         self.hospital_zip = hospital_zip
         self.beds_total = beds_total
         self.beds_occupied = beds_occupied
         self.beds_available = beds_available
+        self.trauma_level = trauma_level
 
        
