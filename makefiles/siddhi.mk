975b98a20ba091716ca84c7e69a945de83bf9c82
Shiraz Amin <shiraz@shirazamin.com>
2020-04-25 13:27:48 -0400

    setup the siddhi runner

--- makefiles/siddhi.mk
+++ makefiles/siddhi.mk
@@ -1,5 +1,8 @@
 siddhi.start: ## Start siddhi in its docker container
+	docker-compose up --build siddhi-runner
+
+siddhi.dev:
 	docker-compose up siddhi
 
 siddhi.daemon: ## Start daemon siddhi in its docker container
-	docker-compose up -d siddhi
+	docker-compose up -d siddhi-runner
