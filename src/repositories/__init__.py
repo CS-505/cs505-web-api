846aa32755a8ba6e2770db53be3145896cc8a6c6
Shiraz Amin <shiraz@shirazamin.com>
2020-04-26 19:46:10 -0400

    zip alert list end point is working

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -2,3 +2,4 @@ from .user import UserRepository
 from .hospital_data import HospitalRepository
 from .zipcode_distance import ZipCodeRepository
 from .patient_data import PatientRepository
+from .zip_alert import ZipAlertRepository
--- src/resources/zip_alert_list.py
+++ src/resources/zip_alert_list.py
@@ -5,6 +5,7 @@ Define the REST verbs relative to the zip alert list
 from flasgger import swag_from
 from flask.json import jsonify
 from flask_restful import Resource
+from repositories import ZipAlertRepository
 
 
 class ZipAlertListResource(Resource):
@@ -12,4 +13,4 @@ class ZipAlertListResource(Resource):
     @staticmethod
     @swag_from("../swagger/zip_alert_list/GET.yml")
     def get():
-        return jsonify({"status": "this works!"})
+        return jsonify(ziplist=ZipAlertRepository.get_alerted_zip_codes())
